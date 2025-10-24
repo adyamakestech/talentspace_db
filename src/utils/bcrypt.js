408733d38a329d5d6dda7cc13d5aed072a29f736
Adya Ginansah <addiaginansahhh@192.168.1.46>
2025-10-24 18:35:13 +0700

    hashing

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,6 +1,8 @@
+import crypto from "crypto";
 import bcrypt from "bcrypt";
 
 export const hashPassword = async (password) => {
+  const customHash = crypto.createHash("sha256").update(password).digest("hex");
   const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(password, salt);
+  return await bcrypt.hash(customHash, salt);
 };
